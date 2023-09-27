045a15a70e7c5a6f3dbde99690cca5ecf144ee66
Baber19i@outlook.com <Baber19i@outlook.com>
2023-09-27 16:12:10 +0500

    Phone OTP Auth

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'phone.dart';
+import 'verify.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: 'phone',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'phone': (context) => const MyPhone(),
+      'verify': (context) => const MyVerify()
+    },
+  ));
+}
